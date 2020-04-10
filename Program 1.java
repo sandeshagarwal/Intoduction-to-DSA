12445c9c5d83bddc991b3007f4e66a8562cad4bd
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-04-10 17:47:19 +0530

    P1 - Sum of first n natural numbers (Commit 2)

--- Program 1.java
+++ Program 1.java
@@ -7,7 +7,11 @@ public class Main
 		System.out.println("Enter the number n");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int sum = n*((n+1)/2); // Direct Formula to calculate sum
+		int sum =0;
+		for(int i=1;i<=n;i++)
+		    for(int j=1;j<=i;j++)
+		        sum ++; // Time Complexity O(n*n) - Worst
+		//int sum = n*((n+1)/2); // Direct Formula to calculate sum (Time Complexity O(1)) -Best
 		System.out.println("The sum is "+ sum);
 	}
 }
